7cc3a3c5a88062e27d2d18ef5a15fe01602fda31
gabykarsten <karstengaby@gmail.com>
2024-03-05 19:40:01 -0800

    edit some instructions to be more user friendly

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "07944d6694a03tbbadbf6e7o423a9f8f";
   let prompt = `Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a funny poem expert and love to write short poems. Your mission is to generate a 4 line poem and seperate each sentence with a <br><br>. Do not include the title. Sign the poem with SheCodes AI at the bottom, underneath the poem inside a <strong> element and not at the beginning. Make sure to follow the user instructions below";
+    "You are a funny funny comedian and love to write short poems. Your mission is to generate a 4 line poem and seperate each sentence with a <br><br>. Do not include the title. Sign the poem with SheCodes AI at the bottom, underneath the poem inside a <strong> element and not at the beginning. Make sure to follow the user instructions below";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
